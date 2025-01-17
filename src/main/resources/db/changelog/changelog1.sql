528c3e409b62676106ec273f415b6319e07a43b8
DevPirimidtech <dev.jariwala@pirimidtech.com>
2025-01-17 11:01:48 +0530

    updated ci/cd file

--- src/main/resources/db/changelog/changelog1.sql
+++ src/main/resources/db/changelog/changelog1.sql
@@ -1,5 +1,5 @@
 -- liquibase formatted sql
 
 -- changeset Dev_Jariwala:1736423103276-2
-CREATE TBLE "account1" ("id" UUID NOT NULL, "account_number" VARCHAR(255), "ifsc_code" VARCHAR(255), CONSTRAINT "account1_pkey" PRIMARY KEY ("id"))
+CRATE TABLE "account1" ("id" UUID NOT NULL, "account_number" VARCHAR(255), "ifsc_code" VARCHAR(255), CONSTRAINT "account1_pkey" PRIMARY KEY ("id"))
 
